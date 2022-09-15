2827418d9005a3c4bbafde12f4b33d824d7fe6da
Carlos Souza <carlos.pessoal@hotmail.com>
2022-09-15 18:16:20 -0300

    feat: use Map instead of plain object in evaluatorService

--- src/evaluatorService.ts
+++ src/evaluatorService.ts
@@ -1,38 +1,42 @@
-type EvaluationOutput = {
-  [describe: string]: 'passed' | 'failed'
-}
+import { createHash } from 'crypto'
 
 const CORRECT_ANSWER_GRADE = 1
 const WRONG_ANSWER_GRADE = 0
+
+const hash = (description: string) => createHash('md5').update(description).digest('hex')
 
 export default () => ({
   evaluateRepository: (params: EvaluateParams): EvaluationResult => {
     const { testResults } = params.resultsFile
     const { requirements } = params.requirementsFile
 
-    const evaluationsByRequirements =
+    const requirementsWithId = requirements.map(({ description }) => ({
+      id: hash(description),
+      description
+    }))
+
+    const requirementsStatusById =
       testResults.map(({ assertionResults }) => (
         assertionResults.map(({ ancestorTitles, status }) => ({
+          id: hash(ancestorTitles[ancestorTitles.length - 1]),
           describe: ancestorTitles[ancestorTitles.length - 1],
           status
         }))
       )).flat()
         .reduce((acc, evaluation) => {
-          const status = acc[evaluation.describe]
-          const currentStatus = evaluation.status
-          if (!status || currentStatus === 'failed') {
-            acc[evaluation.describe] = currentStatus
-            return acc
-          }
+          if (acc.get(evaluation.id) === 'failed') return acc
+
+          acc.set(evaluation.id, evaluation.status)
           return acc
-        }, {} as EvaluationOutput)
+        }, new Map())
 
     const evaluations =
-      requirements.map(({ description }) => ({
+      requirementsWithId.map(({ id, description }) => ({
         description,
-        grade: (evaluationsByRequirements[description] === 'passed') ? CORRECT_ANSWER_GRADE : WRONG_ANSWER_GRADE
+        grade: requirementsStatusById.get(id) === 'passed'
+          ? CORRECT_ANSWER_GRADE
+          : WRONG_ANSWER_GRADE
       }))
-
 
     return {
       github_username: params.githubUsername,
